14d7638db26c430ff3b3d967f783d7c1da9de447
Yida Wang <yidaalex.wang@gmail.com>
2020-04-19 19:16:59 -0400

    create a Layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Layout from "./components/Layout/Layout";
+
 export default function App() {
   return (
     <div>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
 import App from "./App";
 
 import "./index.css";
 
+const engine = new Styletron();
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>,
   rootElement
 );
